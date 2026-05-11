9c3814eb5b064f314031c53b6456df66207da6ae
pjrdgmar001-crypto <pjrdgmar001@gmail.com>
2026-05-11 09:24:57 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChQ9g7pDJVu-LmCJB5rGC_BTfMZk2n17g",
+  authDomain: "pos-pajara.firebaseapp.com",
+  projectId: "pos-pajara",
+  storageBucket: "pos-pajara.firebasestorage.app",
+  messagingSenderId: "926060688939",
+  appId: "1:926060688939:web:889abb30a1f36ada6c9c6e",
+  measurementId: "G-3FZXNV54MG"
+};
+
+firebase.initializeApp(firebaseConfig);
